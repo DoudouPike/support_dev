14fc6d0992ff33bdcf5c43a9ed29ec97617052a7
OssGoku <fafetnach@gmail.com>
2016-08-19 17:08:00 +0200

    ajout du dossier support

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 $error = "";
 
-$page = "home";
-$access = ["home", "login", "html", "cours"]; //ajouter les autres pages
+$page = "creation";
+$access = ["home", "login", "html", "cours", "creation"]; //ajouter les autres pages
 
 if(isset($_GET["page"]) && in_array($_GET["page"], $access))
 {
 	$page = $_GET["page"];
 }
 
-$traitementList = [""];
+$traitementList = ["creation"];
 if(in_array($page, $traitementList))
 {
-	require("controllers/process_" .$page. ".php");
+	require("controllers/traitement_".$page.".php");
 }
 
 require("controllers/skel.php");
