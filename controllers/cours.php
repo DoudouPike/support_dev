fa7f3da21b0b41473dc3f7ca10bfa83709e4d10c
DoudouPike <doudoupike@hotmail.fr>
2016-08-24 16:58:26 +0200

    optimisation nbr page

--- controllers/cours.php
+++ controllers/cours.php
@@ -1,12 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 $json = file_get_contents($_GET["lang"].".json");
 $bdd = json_decode($json, true);
-=======
-$json = file_get_contents($page.".json");
-$stock = json_decode($json, true);
->>>>>>> 62bec9a5717b0fe900c54aec9ff2e688c05f3b53
 
 $bddIntro = $bdd["intro"];
 $bddStep1 = $bdd["step1"];
