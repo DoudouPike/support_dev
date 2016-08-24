a2012952e9945077fa1509680f0ad8e4a3f7396c
DoudouPike <doudoupike@hotmail.fr>
2016-08-24 22:31:30 +0200

    essai pas fini

--- controllers/bloc-cours.php
+++ controllers/bloc-cours.php
@@ -1,24 +1,45 @@
 <?php
 
-if($bdd[] = "intro")
+if(empty($bdd))
 {
-	for($i = 0; $i < sizeof($bddIntro); $i++)
+	$titre = "";
+	$error = "Aucun cours à afficher...";
+}
+else
+{
+	$count = 0;
+	while($count < sizeof($bdd))
+	{
+		$thisbdd = $bdd[$count];
+		if(in_array("intro", $thisbdd))
+		{
+			$categorie = "Introduction";
+			$id = $bdd[$count]["id"];
+			$title = $bdd[$count]["title"];
+			$objectif = $bdd[$count]["objectif"];
+			$contenu = $bdd[$count]["contenu"];
+
+		}
+
+		$count++;
+	}
+
+	/*for($i = 0; $i < sizeof($bddIntro); $i++)
 	{
 		$titre = $bddIntro[$i]["titre"];
 		
 		require("views/bloc-cours.phtml");
-	}	
-}
+	}
 
-if($bdd[] = "step1")
-{
-	for($i = 0; $i < sizeof($bddStep1); $i++)
+	if($bdd[] = "step1")
 	{
-		$titre = $bddStep1[$i]["titre"];
-		
-		require("views/bloc-cours.phtml");
-	}	
+		for($i = 0; $i < sizeof($bddStep1); $i++)
+		{
+			$titre = $bddStep1[$i]["titre"];
+		}	
+	}*/
 }
+require("views/bloc-cours.phtml");
 
 
 ?>
--- controllers/cours.php
+++ controllers/cours.php
@@ -3,11 +3,6 @@
 $json = file_get_contents($_GET["lang"].".json");
 $bdd = json_decode($json, true);
 
-$bddIntro = $bdd["intro"];
-$bddStep1 = $bdd["step1"];
-
-var_dump($bdd);
-
 require("views/cours.phtml");
 
 ?>
