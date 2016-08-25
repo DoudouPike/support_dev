cb60fa95dd76045bd28dc997c944e3468a90133a
ZelusRL <zelus.guillaume@gmail.com>
2016-08-25 16:21:14 +0200

    cours

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $db = mysqli_connect("localhost", "root", "troiswa", "support_dev");
 
 $error = "";
 $page = "home";
-$access = ["home", "login", "logout", "cours", "single", "creation"]; //ajouter les autres pages
+$access = ["home", "login", "logout", "cours", "single", "creation", "404"]; //ajouter les autres pages
 
 if(isset($_GET["page"]) && in_array($_GET["page"], $access))
 {
