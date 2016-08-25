fe7f230f77c660cb4fd760a7cd510cbcd2bb29f9
DoudouPike <doudoupike@hotmail.fr>
2016-08-25 16:48:38 +0200

    single OK

--- controllers/bloc-cours.php
+++ controllers/bloc-cours.php
@@ -10,38 +10,5 @@ while($titleList = mysqli_fetch_assoc($dbLang))
 	$title = $titleList["title"];
 	require("views/bloc-cours.phtml");		
 }
-	/*$count = 0;
-	while($count < sizeof($bdd))
-	{
-		$thisbdd = $bdd[$count];
-
-		if(in_array("intro", $thisbdd))
-		{
-			
-			$id = $thisbdd["id"];
-			$titre = $thisbdd["titre"];
-			$objectif = $thisbdd["objectif"];
-			$contenu = $thisbdd["contenu"];
-
-			$urlTitre = urlencode($titre);
-
-			//$categorie = "Introduction";
-			require("views/bloc-cours.phtml");
-
-		}
-		elseif(in_array("step1", $thisbdd))
-		{
-			
-			$id = $thisbdd["id"];
-			$titre = $thisbdd["titre"];
-			$objectif = $thisbdd["objectif"];
-			$contenu = $thisbdd["contenu"];
-
-			$urlTitre = urlencode($titre);
-			//$categorie = "Step 1";
-			require("views/bloc-cours.phtml");
-		}
-		$count++;
-	}*/
 
 ?>
