468e7ccc5e5e8a4847106ca76e892cf1a2b89e65
OssGoku <fafetnach@gmail.com>
2016-08-26 13:44:01 +0200

    bdd_ok

--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 session_start();
 $db = mysqli_connect("localhost", "root", "troiswa", "support_dev");
 $date = date('D j F Y H:i');
-// $date_post = date();
+
 $error = "";
 $page = "home";
-$access = ["home", "login", "logout", "cours", "single", "creation", "modif", "404"]; //ajouter les autres pages
+$access = ["home", "login", "logout", "cours", "single", "creation", "modif", "404", "delete"]; //ajouter les autres pages
 
 if(isset($_GET["page"]) && in_array($_GET["page"], $access))
 {
 	$page = $_GET["page"];
 }
 
-$traitementList = ["creation", "login", "modif"];
+$traitementList = ["creation", "login", "modif", "delete"];
 
 if(in_array($page, $traitementList))
 {
-	require("controllers/traitement_" .$page. ".php");
+	require("controllers/traitement_".$page.".php");
 }
 
 require("controllers/skel.php");
