fb26ceeda42a30a2d24c773f3889c3f2d0bf0f4a
DoudouPike <doudoupike@hotmail.fr>
2016-08-26 21:44:08 +0200

    coucou

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
+
 <?php
 session_start();
 $db = mysqli_connect("localhost", "root", "troiswa", "support_dev");
-$date = date('D j F Y H:i');
-// $date_post = date();
 $error = "";
 $page = "home";
-$access = ["home", "login", "logout", "cours", "single", "creation", "404"]; //ajouter les autres pages
-
+$access = ["home", "login", "logout", "cours", "single", "creation", "modif", "404", "delete", "register"]; //ajouter les autres pages
 if(isset($_GET["page"]) && in_array($_GET["page"], $access))
 {
 	$page = $_GET["page"];
 }
-
-$traitementList = ["creation", "login"];
-
+else
+{
+	require("controllers/404.php");
+}
+$traitementList = ["creation", "login", "modif", "delete", "register"];
 if(in_array($page, $traitementList))
 {
-	require("controllers/traitement_" .$page. ".php");
+	require("controllers/traitement_".$page.".php");
 }
-
 require("controllers/skel.php");
 ?>
