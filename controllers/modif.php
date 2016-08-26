951dc8261b3c0d8a47f7d7897a8896b90f4c3439
DoudouPike <doudoupike@hotmail.fr>
2016-08-26 16:58:21 +0200

    faille get corrigee

--- controllers/modif.php
+++ controllers/modif.php
@@ -1,17 +1,20 @@
 <?php
 	if(isset($_SESSION["admin"]))
 	{
-		$id = $_GET["id"];
-		$req = "SELECT title,goal,content FROM lessons WHERE id = '".$id."'";
-		$thisDb = mysqli_query($db, $req);
+		if(isset($_GET["id"]) && !empty($_GET["id"]))
+		{
+			$id = $_GET["id"];
+			$req = "SELECT title,goal,content FROM lessons WHERE id = '".$id."'";
+			$thisDb = mysqli_query($db, $req);
 
-		$thisDbTab = mysqli_fetch_assoc($thisDb);
-		
-		$titre = $thisDbTab["title"];
-		$objectif = $thisDbTab["goal"];
-		$contenu = $thisDbTab["content"];
-
-		require("views/modif.phtml");
+			$thisDbTab = mysqli_fetch_assoc($thisDb);
+			
+			require("views/modif.phtml");
+		}
+		else
+		{
+			require("controllers/404.php");
+		}
 	}
 	else
 	{
