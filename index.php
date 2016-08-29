a37d04da633ca5de9ea395f9249bc2334e709f71
ZelusRL <zelus.guillaume@gmail.com>
2016-08-29 13:34:22 +0200

    entities

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 $db = mysqli_connect("localhost", "root", "troiswa", "support_dev");
 $error = "";
 $page = "home";
-$access = ["home", "login", "logout", "cours", "single", "creation", "modif", "404", "delete", "register"]; //ajouter les autres pages
+$access = ["home", "login", "logout", "cours", "single", "creation", "modif", "delete", "register"]; //ajouter les autres pages
 if(isset($_GET["page"]) && in_array($_GET["page"], $access))
 {
 	$page = $_GET["page"];
