22baef642ff277a5e7ce5ee1939c1d1bc7df835f
DoudouPike <doudoupike@hotmail.fr>
2016-08-29 14:50:47 +0200

    maj securité modif.php

--- controllers/modif.php
+++ controllers/modif.php
@@ -5,7 +5,6 @@ if(isset($_GET["id"]) && !empty($_GET["id"]))
 	$req = "SELECT id FROM lessons WHERE id='" .$id. "'";
 	$ids = mysqli_query($db, $req);
 	$idsTab = mysqli_fetch_assoc($ids);
-
 	if($id == $idsTab["id"])
 	{
 		$req = "SELECT title,goal,content FROM lessons WHERE id = '".$id."'";
@@ -15,14 +14,11 @@ if(isset($_GET["id"]) && !empty($_GET["id"]))
 
 		require("views/modif.phtml");
 	}
-
 	else
 	{
 		require("controllers/404.php");
 	}
-
 }
-
 else
 {
 	require("controllers/404.php");
