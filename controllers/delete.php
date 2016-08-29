424261b469d3c0c7753f8766bda6dab4bcc8bd12
OssGoku <fafetnach@gmail.com>
2016-08-29 16:42:39 +0200

    maj_mysqli_escape

--- controllers/delete.php
+++ controllers/delete.php
@@ -1,6 +1,10 @@
 <?php
 	if(isset($_SESSION["admin"]))
 	{
+		if(empty($idSaisi)){
+			header("Location: index.php?page=404");
+			exit;
+		}
 		$idSaisi = intval($_GET['id']);
 		$select_title = mysqli_query($db, "SELECT title, lang FROM lessons WHERE id = '".$idSaisi."'");
 		$title = mysqli_fetch_assoc($select_title);
