4b6808464c360fb2095f17e2a97bbc776433de08
DoudouPike <doudoupike@hotmail.fr>
2016-08-29 15:28:26 +0200

    maj securité modif.php

--- controllers/traitement_register.php
+++ controllers/traitement_register.php
@@ -1,15 +1,34 @@
 <?php
-if (isset($_POST["register"])){
-	$login = $_POST["pseudo"];
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	$password2 = $_POST['password2'];
+if (isset($_POST["register"]))
+{
+	$login = mysqli_real_escape_string($db, $_POST["pseudo"]);
+	$email = mysqli_real_escape_string($db, $_POST["email"]);
+	$password = mysqli_real_escape_string($db, $_POST["password"]);
+	$password2 = mysqli_real_escape_string($db, $_POST["password2"]);
 
-	if (!empty($_POST)) {
-		if($_POST["password"] != $_POST["password2"]){
-			$error = "Merci de saisir des mot de passe identiques";
+	if(empty($login) || empty($email) || empty($password) || empty($password2))
+	{
+		$error = "Merci de compléter tous les champs...";
+	}
+	else
+	{
+		$req = "SELECT email FROM users";
+		$emails = mysqli_query($db, $req);
+
+		while($emailTab = mysqli_fetch_assoc($emails))
+		{
+			if($emailTab["email"] == $email)
+			{
+				$error = "Cet email est déjà utilisé";
+			}
 		}
-		else{
+
+		if($password != $password2)
+		{
+			$error = "Merci de saisir des mots de passe identiques";
+		}
+		else
+		{
 			$res = mysqli_query($db, "INSERT INTO users (login, `email`, `password`) VALUES ('$login', '$email', '$password')");
 			// header("Location: index.php?page=login");
 			// exit;
@@ -27,8 +46,8 @@ if (isset($_POST["register"])){
 			}
 			else{
 
-				header("Location: index.php?page=login");
-				exit;
+				// header("Location: index.php?page=login");
+				// exit;
 				// var_dump($res);
 			}
 
