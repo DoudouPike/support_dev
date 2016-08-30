2ee095ce373c037a0e7458567c027ace160e710e
DoudouPike <doudoupike@hotmail.fr>
2016-08-30 11:46:41 +0200

    initial category

--- controllers/bloc-cours.php
+++ controllers/bloc-cours.php
@@ -1,30 +1,32 @@
 <?php
 	if(isset($_GET["lang"]) && in_array($_GET["lang"], $langAccess))
 	{
-		$lang = mysqli_real_escape_string($db, $_GET["lang"]);
-		$category = mysqli_real_escape_string($db, $_GET["cat"])
+		if(isset($_GET["cat"]) && in_array($_GET["cat"], $langAccess))
+		{
+			$lang = mysqli_real_escape_string($db, $_GET["lang"]);
+			$category = mysqli_real_escape_string($db, $_GET["cat"])
 
-		$req = "SELECT id, title FROM lessons WHERE lang = '".$lang."' ORDER BY category";
+			$req = "SELECT id, title FROM lessons WHERE lang = '".$lang."' ORDER BY category";
 
-		$res = mysqli_query($db, $req);
-		while($blocCoursTab = mysqli_fetch_assoc($res))
-		{	
-			$id = $blocCoursTab["id"];
-		
-			$category
+			$res = mysqli_query($db, $req);
+			while($blocCoursTab = mysqli_fetch_assoc($res))
+			{	
+				$id = $blocCoursTab["id"];
 			
-			$title = $blocCoursTab["title"];
+				$category
+				
+				$title = $blocCoursTab["title"];
 
-			if(isset($_SESSION["admin"]))
-			{
-				require("views/bloc-cours_admin.phtml");
-			}
-			else
-			{
-				require("views/bloc-cours.phtml");
+				if(isset($_SESSION["admin"]))
+				{
+					require("views/bloc-cours_admin.phtml");
+				}
+				else
+				{
+					require("views/bloc-cours.phtml");
+				}
 			}
 		}
-
 	}
 
 	else
