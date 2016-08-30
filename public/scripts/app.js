36acc80e449c095ba6b6605ce50818d234ddb1bf
SoundeluXe87 <husenaumathias@gmail.com>
2016-08-30 14:01:18 +0200

    prettify

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,6 +5,10 @@ tinymce.init({
 });
 
 $(function(){
+	var regex = /<br\s*[\/]?>/gi;
+	$("pre").html($("pre").html().replace(regex, "\n"));
+
+	// $("pre").find("br").replaceWidth("\n");
 	$("pre").addClass("prettyprint linenums");
 	prettyPrint();
 })
